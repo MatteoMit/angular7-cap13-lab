24c7ff238a7ffe30eb347b90270cb4d55df30ec3
MatteoMit <matteo.mitrano@3mlinformatica.it>
2019-12-02 12:19:58 +0100

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms"; // 1
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { TemplateDrivenFormComponent } from "./template-driven-form/template-driven-form.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [AppComponent, TemplateDrivenFormComponent],
+  imports: [
+    BrowserModule,
+    FormsModule // 2
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
