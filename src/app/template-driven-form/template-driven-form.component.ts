c174cee03ff0a3a618dda80e70c04fab63c336df
MatteoMit <matteo.mitrano@3mlinformatica.it>
2019-12-02 15:10:06 +0100

    1 file modified, 1 added

--- src/app/template-driven-form/template-driven-form.component.ts
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -33,12 +33,3 @@ export class TemplateDrivenFormComponent {
     console.log('Form submission');
   }
 }
-export interface Appointment {
-  name: string;
-  mail: string;
-  dayOfTheWeek: string;
-  office: string;
-  application0: boolean;
-  application1: boolean;
-  application2: boolean;
-}
