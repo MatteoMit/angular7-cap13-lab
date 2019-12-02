626260abe6f636dcc093c0b18ca06bd2316b5b70
MatteoMit <matteo.mitrano@3mlinformatica.it>
2019-12-02 12:53:06 +0100

    2 files modified

--- src/app/template-driven-form/template-driven-form.component.ts
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { Appointment } from "../appointment";
 @Component({
-  selector: 'simple-template-driven-form',
-  templateUrl: './template-driven-form.component.html',
-  styleUrls: ['./template-driven-form.component.css']
+  selector: "simple-template-driven-form",
+  templateUrl: "./template-driven-form.component.html",
+  styleUrls: ["./template-driven-form.component.css"]
 })
-export class TemplateDrivenFormComponent implements OnInit {
+export class TemplateDrivenFormComponent {
+  daysOfTheWeek = ["Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì"];
 
-  constructor() { }
+  availableOffices = [
+    { id: "A", name: "ufficio_A" },
+    { id: "B", name: "ufficio_B" },
+    { id: "C", name: "ufficio_C" }
+  ];
 
-  ngOnInit() {
-  }
-
+  applications = [{ id: 0 }, { id: 1 }, { id: 2 }];
+  
+  model: Appointment = {
+    name: "",
+    mail: "",
+    dayOfTheWeek: "Lunedì",
+    office: "ufficio_A",
+    application0: false,
+    application1: false,
+    application2: false
+  };
 }
