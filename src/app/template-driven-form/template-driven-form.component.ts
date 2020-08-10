22ddecf9deec8d8559aab1f00a22240bc033c7be
MatteoMit <matteo.mitrano@3mlinformatica.it>
2020-08-10 12:53:39 +0200

    1 file modified

--- src/app/template-driven-form/template-driven-form.component.ts
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { Appointment } from "../appointment";
+
 @Component({
   selector: "simple-template-driven-form",
   templateUrl: "./template-driven-form.component.html",
